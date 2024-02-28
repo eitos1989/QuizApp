b67aa5d426241350e465d76b4cc09ba36bfdef2e
Erik Thomsen <erikthomsen1989@gmail.com>
2024-02-28 08:17:54 +0100

    QuizApp and JSON Data Structure

---
+++ variables.js
@@ -0,0 +1,42 @@
+let questions =[
+    {
+        "question" : "Wer hat HTML erfunden",
+        "answer_1" : "Robbie Williams",
+        "answer_2" : "Lady Gaga",
+        "answer_3" : "Tim Berners-Lee",
+        "answer_4" : "Justin Bieber",
+        "right_answer" : 3
+    },
+    {
+        "question" : "Die erste scharfe Weltraumaufnahme von der Erde lieferte ein Wettersatellit. In welchem Jahr war das?",
+        "answer_1" : "1966",
+        "answer_2" : "1960",
+        "answer_3" : "1972",
+        "answer_4" : "1963",
+        "right_answer" : 2
+    },
+    {
+        "question" : 'Für die Sowjetunion war 1961 die Mission "Wostok 1" ein grandioser Erfolg über den Erzfeind USA. Was war "Wostok 1"?',
+        "answer_1" : "das erste bemannte Raumschiff",
+        "answer_2" : "die erste Raumfähre ",
+        "answer_3" : "der erste Weltraumsatellit",
+        "answer_4" : "die erste Mission zum Mond",
+        "right_answer" : 1
+    },    
+    {
+        "question" : 'Das US-Militär schuf 1942 das "Manhattan Project". Was war das Ziel der beteiligten Wissenschaftler?',
+        "answer_1" : "die Bekämpfung der Slums",
+        "answer_2" : "der Bau einer Weltraumrakete",
+        "answer_3" : "der Bau der längsten Bahnversorgungsstrecke",
+        "answer_4" : "der Bau der Atombombe",
+        "right_answer" : 4
+    },
+    {
+        "question" : 'Die erste brauchbare elektrische Lampe brannte 1879. Wer hatte sie erfunden?',
+        "answer_1" : "George Eastman",
+        "answer_2" : "Thomas Alva Edison",
+        "answer_3" : "James Watt",
+        "answer_4" : "James Dien",
+        "right_answer" : 2
+    }
+];
