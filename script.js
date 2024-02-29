e5e5edc443242a890aee65a325675a4941cc4e10
Erik Thomsen <erikthomsen1989@gmail.com>
2024-03-01 00:39:03 +0100

    QuizApp is working in general

---
+++ script.js
@@ -0,0 +1,44 @@
+let curQuestion = 0;
+
+function init() {
+    document.getElementById('questionCnt').innerHTML = questions.length;
+    showQuestion();
+}
+
+function showQuestion(){
+    let question = questions[curQuestion];
+    document.getElementById('questionCur').innerHTML = question.question;
+    document.getElementById('answer_1').innerHTML = question.answer_1;
+    document.getElementById('answer_2').innerHTML = question.answer_2;
+    document.getElementById('answer_3').innerHTML = question.answer_3;
+    document.getElementById('answer_4').innerHTML = question.answer_4;
+    
+}
+
+function answer(answerID) {
+    let question = questions[curQuestion];
+    let selectedQuestionNumber = answerID.slice(-1);
+    let idofRightAnswer = "answer_" + question.right_answer;
+    if(question.right_answer == selectedQuestionNumber){
+        document.getElementById(answerID).parentNode.classList.add("bg-success");
+    }else{
+        document.getElementById(answerID).parentNode.classList.add("bg-danger");
+        document.getElementById(idofRightAnswer).parentNode.classList.add("bg-success");
+    }
+    document.getElementById('next-button').disabled = false;
+}
+
+function nextQuestion() {
+    curQuestion++;
+    resetAnswerButtons();
+    showQuestion();
+    document.getElementById('curQuestionCnt').innerHTML = curQuestion+1;
+    document.getElementById('next-button').disabled = true;
+}
+
+function resetAnswerButtons() {
+    for (let i = 1; i < 5; i++) {
+        document.getElementById(`answer_${i}`).parentNode.classList.remove("bg-success");
+        document.getElementById(`answer_${i}`).parentNode.classList.remove("bg-danger");
+    }
+}
--- variables.js
+++ variables.js
@@ -1,6 +1,6 @@
 let questions =[
     {
-        "question" : "Wer hat HTML erfunden",
+        "question" : "Wer hat HTML erfunden?",
         "answer_1" : "Robbie Williams",
         "answer_2" : "Lady Gaga",
         "answer_3" : "Tim Berners-Lee",
